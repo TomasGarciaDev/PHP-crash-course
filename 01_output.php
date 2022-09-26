eb653ecfc27c89152394d5ec1b25fc8ce482f17d
Tomas Garcia <tomasgarciadev@gmail.com>
2022-09-26 10:38:51 +0200

    First commit

---
+++ 01_output.php
@@ -0,0 +1,16 @@
+<?php 
+// echo - Output strings, numbers, html, etc
+  // echo 'Hello', 123, 10.5;
+  
+// print - Works llike echo, but can only take in a single argument
+  // print 123;
+
+// print_r() - Print songle values and arrays
+  // print_r([1, 2, 3]);
+
+// var_dump() - Returns more info like data type and length
+    // var_dump(true):
+
+// var_export() - Similar to var_dump(). Outputs a string representation of a varianble
+  // var_export('Hello')
+?>
